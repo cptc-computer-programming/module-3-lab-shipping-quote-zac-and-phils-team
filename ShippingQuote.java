32064600d9fe0023ef664c43a0fb8a2fe18337d0
PhilNgu123 <pnguyen5827@students.cptc.edu>
2026-01-22 11:45:46 -0800

    Add base formula to ShippingQuote

---
+++ ShippingQuote.java
@@ -0,0 +1,22 @@
+public class ShippingQuote {
+   public static void main(String[] args) {
+    // Weightinpounds();
+    // Lengthininches();
+    // Widthininches();
+    // Heightininches();
+
+   } 
+   
+   totalCost = baseCost + (weight *weightRate) + (sizeFactor* sizeRate)
+   sizeFactor = int max(length, width, height);
+    return length + width + height;
+
+
+    public static void sizeFactor() {
+        
+
+    }
+
+
+}
+    
